d27c1c3e2f4e2688aeb761f59db4fcaa7c28ae08
Sathiyapramod <sathiyapramod22@gmail.com>
2023-04-21 18:02:41 +0530

    *

--- src/Components/Appbar.js
+++ src/Components/Appbar.js
@@ -4,46 +4,38 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 import { LinkStyling } from "../General/General";
 
 export default function Appbar() {
+  console.log(localStorage.getItem("usertype"));
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" color="secondary">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            WELCOME TO CRM app
+            CRM Web app
           </Typography>
           <Button color="inherit">Login</Button>
-          <Link to="/dashboard" style={LinkStyling}>
-            <Button color="inherit">DASHBOARD</Button>
-          </Link>
-          <Link to="/adduser" style={LinkStyling}>
-            {localStorage.getItem("usertype") !== "employee" && (
-              <Button color="inherit">Add User</Button>
-            )}
-          </Link>
-          <Button
-            color="inherit"
-            onClick={() => {
-              localStorage.clear();
-              window.location.href = "/";
-            }}
-          >
-            Logout
-          </Button>
+          <span>
+            <Link to="/dashboard" style={LinkStyling}>
+              <Button color="inherit">DASHBOARD</Button>
+            </Link>
+            <Link to="/adduser" style={LinkStyling}>
+              {localStorage.getItem("usertype") !== "employee" && (
+                <Button color="inherit">Add User</Button>
+              )}
+            </Link>
+            <Button
+              color="inherit"
+              onClick={() => {
+                localStorage.clear();
+                window.location.href = "/";
+              }}
+            >
+              Logout
+            </Button>
+          </span>
         </Toolbar>
       </AppBar>
     </Box>
---
+++ src/Components/DashboardContent/Usertable.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Usertable() {
+  return (
+    <div>Usertable</div>
+  )
+}
+
+export default Usertable
