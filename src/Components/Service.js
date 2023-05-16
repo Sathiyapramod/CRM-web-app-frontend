b76469c4195b084dae05c11c2724e5ef6fdd447c
Sathiyapramod <sathiyapramod22@gmail.com>
2023-05-16 18:11:42 +0530

    added changes

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,22 +1,38 @@
 import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
-import Table from "@mui/material/Table";
 import { useNavigate } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
-import {
-  Paper,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
+import { Paper } from "@mui/material";
 import { API } from "../General/General";
+import { DataGrid } from "@mui/x-data-grid";
 
 function Service() {
   const navigate = useNavigate();
   const [services, setService] = useState([]);
   const [status, setStatus] = useState("created");
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "Sl.no.",
+      width: 70,
+    },
+    {
+      field: "date",
+      headerName: "date",
+      width: 180,
+    },
+    {
+      field: "name",
+      headerName: "name",
+      width: 180,
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      width: 200,
+    },
+  ];
 
   const getServices = () => {
     fetch(`${API}/service/`, {
@@ -29,6 +45,7 @@ function Service() {
       .then((response) => response.json())
       .then((result) => {
         setService(result);
+        console.log(result);
       });
   };
 
@@ -41,108 +58,20 @@ function Service() {
           className="mx-auto mt-5 rounded-3 pb-3"
           elevation={6}
         >
-          <TableContainer>
-            <Table sx={{ minWidth: 300 }}>
-              <TableHead>
-                <TableRow>
-                  <TableCell
-                    colSpan={5}
-                    className="fs-5 bg-light fw-bolder"
-                    align="center"
-                  >
-                    Service Requests
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  {[
-                    "#",
-                    "Request Name",
-                    "Description",
-                    "Date",
-                    "See Workflow Action",
-                  ].map((element, index) => {
-                    return (
-                      <TableCell
-                        key={index}
-                        align="center"
-                        className="fw-bolder fs-5"
-                      >
-                        {element}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {services.map((user, index) => {
-                  return (
-                    <TableRow key={index}>
-                      <TableCell align="center">{index + 1}</TableCell>
-                      <TableCell align="center">{user.name}</TableCell>
-                      <TableCell align="center">{user.description}</TableCell>
-                      <TableCell align="center">
-                        {user.date.slice(0, 10)}
-                      </TableCell>
-                      <TableCell align="center">
-                        {localStorage.getItem("usertype") !== "employee" ? (
-                          <Button
-                            color="primary"
-                            variant="contained"
-                            onClick={() => {
-                              setStatus("Completed");
-                              const newStatus = { Status: status };
-                              fetch(`${API}/service/${user._id}`, {
-                                method: "PUT",
-                                body: JSON.stringify(newStatus),
-                                headers: {
-                                  "Content-type": "application/json",
-                                  "x-auth-token": localStorage.getItem("token"),
-                                },
-                              })
-                                .then((response) => response.json())
-                                .then(() => {
-                                  alert(`Approved Request no. ${user._id}`);
-                                  getServices();
-                                });
-                            }}
-                          >
-                            Approve
-                          </Button>
-                        ) : (
-                          <Button color="primary" variant="contained" disabled>
-                            Approve
-                          </Button>
-                        )}
-                        {localStorage.getItem("usertype") !== "employee" && (
-                          <Button
-                            color="error"
-                            onClick={() => {
-                              setStatus("Cancelled");
-                              const newStatus = { Status: status };
-                              fetch(`${API}/service/${user._id}`, {
-                                method: "POST",
-                                body: JSON.stringify(newStatus),
-                                headers: {
-                                  "x-auth-token": localStorage.getItem("token"),
-                                },
-                              })
-                                .then((response) => response.json())
-                                .then(() => {
-                                  alert(`Deleted Request no. ${user._id}`);
-                                  getServices();
-                                });
-                            }}
-                          >
-                            Cancel
-                          </Button>
-                        )}
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <DataGrid
+            rows={services}
+            
+            getRowId={(row) => row._id}
+            columns={columns}
+            className="fw-bold fs-5"
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+          ></DataGrid>
           <br />
           <Button
             variant="contained"
